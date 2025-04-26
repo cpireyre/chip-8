e3327da000f46ec23fb00b6eeff26990df6175d0
Colin <cpireyre@student.42.fr>
2025-04-26 20:29:14 +0300

    Boot.

--- main.c
+++ main.c
@@ -4,35 +4,63 @@
 #include <string.h>
 #include <stdint.h>
 
-# define RAM_SIZE_BYTES	4096
-# define CLS 0x00e0
+# define SIZE	4096
 
-static void	show(uint8_t *mem);
+typedef struct
+{
+	uint8_t		mem[SIZE];
+	uint8_t		screen[64 * 32];
+	uint8_t		reg[18];
+	uint16_t	I;
+	uint16_t	pc;
+	uint16_t	stack[16];
+	uint8_t		top;
+} Chip;
+
+# define CLS 0x00e0
+uint16_t swap(uint16_t x) { return (x >> 8 | x << 8); }
+
+static void	boot(Chip *chip);
 static int	load(uint8_t *mem, const char *path);
+static void dump(Chip *chip);
+static void	show(uint64_t *screen);
 
 int	main(int argc, char **argv)
 {
-	uint8_t	mem[RAM_SIZE_BYTES];
+	Chip	chip;
 	int		err;
 
 	if (argc != 2) { printf("Usage: %s <rom.ch8>\n", argv[0]); return (1); }
 	printf("I want to display the memory.\n");
 
-	err = load(mem, argv[1]);
+	boot(&chip);
+	err = load(chip.mem, argv[1]);
 	if (err)
 		return (1);
-	show(mem);
+	dump(&chip);
+	show(chip.screen);
 	return (0);
 }
 
-uint16_t	swap(uint16_t x) { return (x >> 8 | x << 8); }
+static void	show(uint64_t *screen)
+{
+	(void)screen;
+}
 
-static void show(uint8_t *mem)
+static void	boot(Chip *chip)
+{
+	bzero(chip, sizeof(*chip));
+	chip->pc = 0x200;
+}
+
+static void dump(Chip *chip)
 {
     int 					i, j;
     uint16_t 				value;
     static const uint8_t	zeros[16];
+	uint8_t					*mem;
 
+	mem = chip->mem;
     for (i = 0x200; i < 0x400; i += 16)
     {
         if (memcmp(mem + i, zeros, 16) == 0)
@@ -42,10 +70,12 @@ static void show(uint8_t *mem)
         for (j = 0; j < 16; j += 2)
         {
             value = (mem[i + j] << 8) | mem[i + j + 1];
-			if (value == CLS)
-				printf("\x1b[0;32m");
-			printf("%04x ", value);
-			printf("\x1b[0m");
+			if (i + j == chip->pc)
+				printf("\x1b[0;7m");
+			printf("%04x", value);
+			if (i + j == chip->pc)
+				printf("\x1b[0;27m");
+			printf(" ");
         }
         printf("\n");
     }
@@ -63,8 +93,7 @@ static int	load(uint8_t *mem, const char *path)
 		perror("open");
 		return (1);
 	}
-	bzero(mem, 4096);
-	program_data_bytes = read(program_fd, mem + 512, RAM_SIZE_BYTES - 512);
+	program_data_bytes = read(program_fd, mem + 512, SIZE - 512);
 	if (program_data_bytes < 0)
 	{
 		perror("read");
