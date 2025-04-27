e9f59817a323cfe1a275dfe65285b23e3569dcb6
Colin <cpireyre@student.42.fr>
2025-04-27 12:07:39 +0300

    Drawing circles on the screen!

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
@@ -9,7 +10,7 @@
 typedef struct
 {
 	uint8_t		mem[SIZE];
-	uint8_t		screen[64 * 32];
+	uint8_t		screen[256];
 	uint8_t		reg[18];
 	uint16_t	I;
 	uint16_t	pc;
@@ -23,7 +24,7 @@ uint16_t swap(uint16_t x) { return (x >> 8 | x << 8); }
 static void	boot(Chip *chip);
 static int	load(uint8_t *mem, const char *path);
 static void dump(Chip *chip);
-static void	show(uint64_t *screen);
+static void	show(uint8_t *screen);
 
 int	main(int argc, char **argv)
 {
@@ -31,20 +32,56 @@ int	main(int argc, char **argv)
 	int		err;
 
 	if (argc != 2) { printf("Usage: %s <rom.ch8>\n", argv[0]); return (1); }
-	printf("I want to display the memory.\n");
 
 	boot(&chip);
 	err = load(chip.mem, argv[1]);
 	if (err)
 		return (1);
-	dump(&chip);
+	(void)dump;
+	/* dump(&chip); */
 	show(chip.screen);
 	return (0);
 }
 
-static void	show(uint64_t *screen)
+static void	blink(uint8_t *screen, uint8_t x, uint8_t y)
 {
-	(void)screen;
+	screen[x >> 3 | y << 3] ^= 1 << (x & 7);
+}
+
+static void	show(uint8_t *screen)
+{
+	uint8_t		row, col;
+	uint8_t		index;
+	uint32_t	distance;
+	uint8_t		centerx, centery;
+	uint8_t		radius;
+	uint8_t		square;
+
+	centerx = 18;
+	centery = 17;
+	radius = 4;
+	square = radius * radius;
+	for (row = 0; row < 32; row++)
+	{
+		for (col = 0; col < 64; col++)
+		{
+			distance = (col - centerx) * (col - centerx) + (row - centery) * (row - centery);
+			if (0 <= distance - square && distance - square <= 16)
+				blink(screen, col, row);
+		}
+	}
+	for (row = 0; row < 32; row++)
+	{
+		for (col = 0; col < 64; col++)
+		{
+			index = col >> 3 | row << 3;
+			if ((screen[index] >> (col & 7)) & 1)
+				write(1, "1", 1);
+			else
+				write(1, "0", 1);
+		}
+		write(1, "\n", 1);
+	}
 }
 
 static void	boot(Chip *chip)
@@ -85,7 +122,6 @@ static int	load(uint8_t *mem, const char *path)
 {
 	int program_fd;
 	int program_data_bytes;
-	printf("We need to load the code in %s\n", path);
 
 	program_fd = open(path, O_RDONLY);
 	if (program_fd < 0)
@@ -101,6 +137,5 @@ static int	load(uint8_t *mem, const char *path)
 		return (1);
 	}
 	close(program_fd);
-	printf("Loaded code into memory. May have truncated.\n");
 	return (0);
 }
